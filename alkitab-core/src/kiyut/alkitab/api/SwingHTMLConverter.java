88007a4ea68a2d00396516db5f867c570f647c2a
Tonny Kohar <tonny@kiyut.com>
2012-01-17 23:40:29 +0700

    Workaround for Java 7 and xalan use of extension function 'http://xml.apache.org/xalan/java:instance' is not allowed when the secure processing feature is set to true. By using reflection - Create new TransformerFactory - set isNotSecureProcessing to true - Replace the TransformingSAXEventProvider.transfact with the above

--- alkitab-core/src/kiyut/alkitab/api/SwingHTMLConverter.java
+++ alkitab-core/src/kiyut/alkitab/api/SwingHTMLConverter.java
@@ -1,10 +1,13 @@
 /* This work has been placed into the public domain. */
-
 package kiyut.alkitab.api;
 
+import java.lang.reflect.Field;
 import java.net.URL;
 import java.util.MissingResourceException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
 import org.crosswire.common.util.NetUtil;
 import org.crosswire.common.util.ResourceUtil;
 import org.crosswire.common.xml.Converter;
@@ -13,8 +16,8 @@ import org.crosswire.common.xml.TransformingSAXEventProvider;
 
 /**
  * Simple Swing-HTML stylesheet converter
- *  
- * 
+ *
+ *
  */
 public class SwingHTMLConverter implements Converter {
 
@@ -22,10 +25,11 @@ public class SwingHTMLConverter implements Converter {
     public static String BASE_URL = "baseURL";
     public static String FONT = "font";
     //public static String CSS = "css";
+    /**
+     * The xsl path , by default it is kiyut/alkitab/modules/jsword/xsl/simple.xsl
+     */
+    private static String xslPath = "kiyut/alkitab/modules/jsword/xsl/simple.xsl";
 
-    /** The xsl path , by default it is kiyut/alkitab/modules/jsword/xsl/simple.xsl */
-    private static String xslPath = "kiyut/alkitab/modules/jsword/xsl/simple.xsl"; 
-    
     @Override
     public SAXEventProvider convert(SAXEventProvider xmlsep) throws TransformerException {
         try {
@@ -33,12 +37,33 @@ public class SwingHTMLConverter implements Converter {
             URL xslURL = ResourceUtil.getResource(path);
 
             TransformingSAXEventProvider tsep = new TransformingSAXEventProvider(NetUtil.toURI(xslURL), xmlsep);
-            
+
+            /* XXX: workaround for Java 7 and xalan use of extension function 'http://xml.apache.org/xalan/java:instance'
+             * is not allowed when the secure processing feature is set to true.
+             * By using reflection
+             * - Create new TransformerFactory
+             * - set isNotSecureProcessing to true 
+             * - Replace the TransformingSAXEventProvider.transfact with the above 
+             */
+            try {
+                TransformerFactory tf = TransformerFactory.newInstance();
+                Field _isNotSecureProcessing = tf.getClass().getDeclaredField("_isNotSecureProcessing");
+                _isNotSecureProcessing.setAccessible(true);
+                _isNotSecureProcessing.set(tf, Boolean.TRUE);
+                
+                Field _transfact = tsep.getClass().getDeclaredField("transfact");
+                _transfact.setAccessible(true);
+                _transfact.set(tsep,tf);
+                
+            } catch (Exception ex) {
+                Logger logger = Logger.getLogger(this.getClass().getName());
+                logger.log(Level.WARNING, ex.getMessage());
+            }
+
             return tsep;
-            
+
         } catch (MissingResourceException ex) {
             throw new TransformerException(ex);
         }
     }
-
 }
