1ab86ad5afac96db1beb52d2b4814bc0be1f8a78
Tonny Kohar <tonny@kiyut.com>
2012-01-18 13:31:36 +0700

    Workaround for Java 7 and xalan use of extension function 'http://xml.apache.org/xalan/java:instance' is not allowed when the secure processing feature is set to true. Fix by using reflection

--- alkitab-core/src/kiyut/alkitab/api/SwingHTMLConverter.java
+++ alkitab-core/src/kiyut/alkitab/api/SwingHTMLConverter.java
@@ -37,33 +37,54 @@ public class SwingHTMLConverter implements Converter {
             URL xslURL = ResourceUtil.getResource(path);
 
             TransformingSAXEventProvider tsep = new TransformingSAXEventProvider(NetUtil.toURI(xslURL), xmlsep);
-
-            /* XXX: workaround for Java 7 and xalan use of extension function 'http://xml.apache.org/xalan/java:instance'
-             * is not allowed when the secure processing feature is set to true.
-             * By using reflection
-             * - Create new TransformerFactory
-             * - set isNotSecureProcessing to true 
-             * - Replace the TransformingSAXEventProvider.transfact with the above 
-             */
-            try {
-                TransformerFactory tf = TransformerFactory.newInstance();
-                Field _isNotSecureProcessing = tf.getClass().getDeclaredField("_isNotSecureProcessing");
-                _isNotSecureProcessing.setAccessible(true);
-                _isNotSecureProcessing.set(tf, Boolean.TRUE);
-                
-                Field _transfact = tsep.getClass().getDeclaredField("transfact");
-                _transfact.setAccessible(true);
-                _transfact.set(tsep,tf);
-                
-            } catch (Exception ex) {
-                Logger logger = Logger.getLogger(this.getClass().getName());
-                logger.log(Level.WARNING, ex.getMessage());
-            }
+            fixForJava7(tsep);
 
             return tsep;
 
         } catch (MissingResourceException ex) {
             throw new TransformerException(ex);
         }
+    }
+
+    /**
+     * XXX Workaround for Java 7 and xalan use of extension function 'http://xml.apache.org/xalan/java:instance' is not
+     * allowed when the secure processing feature is set to true. *
+     *
+     */
+    protected void fixForJava7(TransformingSAXEventProvider tsep) {
+        // use reflection to set the TransformerFactory private field _isNotSecureProcessing to true
+        // thanks to Brian Fernandes (author of FireBible)
+        try {
+            Field field = tsep.getClass().getDeclaredField("transfact");
+            field.setAccessible(true);
+            Object transfact = field.get(tsep);
+            Field _isNotSecureProcessing = transfact.getClass().getDeclaredField("_isNotSecureProcessing");
+            _isNotSecureProcessing.setAccessible(true);
+            _isNotSecureProcessing.set(transfact, Boolean.TRUE);
+        } catch (Exception ex) {
+            Logger logger = Logger.getLogger(this.getClass().getName());
+            logger.log(Level.WARNING, ex.getMessage());
+        }
+
+        /*
+         * XXX: workaround for Java 7 and xalan use of extension function 'http://xml.apache.org/xalan/java:instance' is
+         * not allowed when the secure processing feature is set to true. 
+         * By using reflection 
+         * - Create new TransformerFactory 
+         * - set isNotSecureProcessing to true (reflection) 
+         * - Replace the TransformingSAXEventProvider.transfact with the above (reflection)
+         */
+        /*
+         * try { TransformerFactory tf = TransformerFactory.newInstance(); Field _isNotSecureProcessing =
+         * tf.getClass().getDeclaredField("_isNotSecureProcessing"); _isNotSecureProcessing.setAccessible(true);
+         * _isNotSecureProcessing.set(tf, Boolean.TRUE);
+         *
+         * Field _transfact = tsep.getClass().getDeclaredField("transfact"); _transfact.setAccessible(true);
+         * _transfact.set(tsep,tf);
+         *
+         * } catch (Exception ex) { Logger logger = Logger.getLogger(this.getClass().getName());
+         * logger.log(Level.WARNING, ex.getMessage()); }
+         *
+         */
     }
 }
