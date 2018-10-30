dcc463aa133a9064838e3c93a2e19bde669d50bd
Tonny <none@none>
2018-10-30 20:53:08 +0700

    Merge Xalan module into JSword module

--- jsword/src/kiyut/alkitab/modules/jsword/Installer.java
+++ jsword/src/kiyut/alkitab/modules/jsword/Installer.java
@@ -1,17 +1,12 @@
 /* This work has been placed into the public domain. */
-
-package kiyut.alkitab.modules.xalan;
+package kiyut.alkitab.modules.jsword;
 
 import org.openide.modules.ModuleInstall;
 
-/**
- *
- * @author tonny
- */
-public class XalanModuleInstall extends ModuleInstall {
-    
+public class Installer extends ModuleInstall {
+
     @Override
-    public  void restored() {
+    public void restored() {
         super.restored();
         
         // Intrepetive Transformer org.apache.xalan.processor.TransformerFactoryImpl
@@ -21,6 +16,6 @@ public class XalanModuleInstall extends ModuleInstall {
         System.setProperty("javax.xml.transform.TransformerFactory", "org.apache.xalan.xsltc.trax.SmartTransformerFactoryImpl");
         System.setProperty("javax.xml.parsers.DocumentBuilderFactory", "org.apache.xerces.jaxp.DocumentBuilderFactoryImpl");
         System.setProperty("javax.xml.parsers.SAXParserFactory", "org.apache.xerces.jaxp.SAXParserFactoryImpl");
-        
     }
+
 }
