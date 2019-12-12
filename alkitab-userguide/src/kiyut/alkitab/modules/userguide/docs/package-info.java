0cfed191808e0a05f8093811656339fc1d9625c0
Tonny Kohar <13114803+tonny-kohar@users.noreply.github.com>
2019-12-12 20:04:10 +0700

    User Guide registration using annotation instead of xml

---
+++ alkitab-userguide/src/kiyut/alkitab/modules/userguide/docs/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@HelpSetRegistration(helpSet = "userguide-hs.xml", position = 3452)
+package kiyut.alkitab.modules.userguide.docs;
+
+import org.netbeans.api.javahelp.HelpSetRegistration;
